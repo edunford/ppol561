7eaa89df4802f3906f19183a130c7e5f777ce93a
Eric Dunford <eric.dunford@georgetown.edu>
2020-04-07 16:50:31 -0400

    Add week 12 lecture materials.

---
+++ Lectures/week_12/macros.js
@@ -0,0 +1,34 @@
+/**
+
+Macros
+
+**/
+
+/** Spacing  **/
+remark.macros.space = function(percentage) {
+  return `<div style = "height: ${percentage}%"></div>`;
+};
+
+/** Image Orientation  **/
+
+remark.macros.center_img = function(widthpercent) {
+  const path = this;
+  return `<div style="text-align:center;">
+            <img src='${path}' width = ${widthpercent}%/>
+          </div>`;
+};
+
+remark.macros.text_color = function(color) {
+  const text = this;
+  return `<span style="color:${color}">${text}</span>`;
+};
+
+
+remark.macros.text_size = function(size) {
+  const text = this;
+  return `<font size="${size}">${text}</font>`;
+};
+
+
+
+
